fd87042fa413d6233e54f166bbd23a9e28a82426
kieulong2003 <91303386+kieulong2003@users.noreply.github.com>
2021-09-24 11:49:42 +0700

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("babe i know ai chang co giac mo ca doi\n");
+	printf("anh di noi dau cung chi muon co em cho doi\n");
+	printf("vi nho doi mat nau tren cao voi voi\n");
+	printf("tam tri anh choi voi cho xin cau tra loi\n");
+	
+	return 0;  
+	
+}
